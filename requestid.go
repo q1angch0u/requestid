ad6c9d8a14f1839f25e18561d430a1082f05f203
burui <burui@integritytech.com.cn>
2022-04-20 15:09:53 +0800

    add code

---
+++ requestid.go
@@ -0,0 +1,42 @@
+package requestid
+
+import (
+	"github.com/petermattis/goid"
+	"sync"
+)
+
+var (
+	requestIDs = map[int64]interface{}{}
+	rwm        sync.RWMutex
+)
+
+// Set 设置一个 RequestID
+func Set(ID interface{}) {
+	goID := getGoID()
+	rwm.Lock()
+	defer rwm.Unlock()
+
+	requestIDs[goID] = ID
+}
+
+// Get 返回设置的 RequestID
+func Get() interface{} {
+	goID := getGoID()
+	rwm.RLock()
+	defer rwm.RUnlock()
+
+	return requestIDs[goID]
+}
+
+// Delete 删除设置的 RequestID
+func Delete() {
+	goID := getGoID()
+	rwm.Lock()
+	defer rwm.Unlock()
+
+	delete(requestIDs, goID)
+}
+
+func getGoID() int64 {
+	return goid.Get()
+}
